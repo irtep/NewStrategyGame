30ec5a4d5963cbc98f357beb300e6b13d47d8448
Glitch (thenewgame) <none>
2018-10-08 21:04:38 +0000

    update9oct

---
+++ public/orders.js
@@ -0,0 +1,17 @@
+// ORDERS:
+
+// standby (do not move, but shoot closest enemy in LOS and in range)
+
+// hunt to shoot (start to move towards target in order to get in range an in LOS. stop moving when in range)
+
+// hunt to assault (start to move towards target in order to engage in melee)
+
+// move to direction (move to w,nw,n,ne,e,se,s,sw. shoot targets while moving)
+
+// shoot target, but keep position. (shoot target, but dont chase if target out of los/range)
+
+// embark to transport (embark to transport that is near)
+
+// disembark troops (disembard troops from transport)
+
+// join squad (characters only. join squad that is near)
