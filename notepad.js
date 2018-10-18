272490704e6391d306ce298963b1eb67eb18798d
Glitch (thenewgame) <none>
2018-10-18 02:06:12 +0000

    update 18oct18 morning

--- notepad.js
+++ notepad.js
@@ -1,5 +1,7 @@
 /*
 BUGS:
 - ranger unit for some reason walks through walks... check stats etc...
+  it seems to be happening when unit size is odds.
+  
 - its impossible to walk away from contact with unit.
 */
